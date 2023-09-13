ccc5e00b69288b86c58d4a1f8aefb6f48a87aefa
Yasholo <yashk5635@gmail.com>
2023-09-13 16:24:53 +0530

    new commit

---
+++ DevOps/Linux-for-Devops/01.hello.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+#Creates a new variable with a value of "Hello World"
+learningbash="Hello World"
+echo $learningbash
---
+++ DevOps/Linux-for-Devops/15.concatenation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+firststring="The secret is..."
+secondstring="Bash"
+thirdstring="$firststring$secondstring"
+echo "$thirdstring"
+
+#!/bin/bash
+#firststring="The secret is..."
+#firststring+="Bash"
+#echo "$firststring" 
